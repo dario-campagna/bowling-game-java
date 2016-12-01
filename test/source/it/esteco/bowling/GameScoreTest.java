954ea8026397022138ec165954ed923dba04c0bd
dcampagna <campagna@esteco.com>
2016-12-01 16:39:05 +0100

    Removed duplication in tests.

--- test/source/it/esteco/bowling/GameScoreTest.java
+++ test/source/it/esteco/bowling/GameScoreTest.java
@@ -1,28 +1,34 @@
 package it.esteco.bowling;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GameScoreTest {
 
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            game.roll(pins);
+        }
+    }
+
     @Test
     public void gutterGame() throws Exception {
-        Game game = new Game();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(20, 0);
         assertEquals(0, game.score());
     }
 
     @Test
     public void allOnes() throws Exception {
-        Game game = new Game();
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
-
+        rollMany(20, 1);
         assertEquals(20, game.score());
     }
 }
