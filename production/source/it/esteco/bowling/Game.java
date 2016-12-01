289eed420ce9d4a9610f654156d2c5e425e2b88c
dcampagna <campagna@esteco.com>
2016-12-01 16:32:37 +0100

    We now compute the score of a gutter game.

---
+++ production/source/it/esteco/bowling/Game.java
@@ -0,0 +1,10 @@
+package it.esteco.bowling;
+
+public class Game {
+    public int score() {
+        return 0;
+    }
+
+    public void roll(int pins) {
+    }
+}
---
+++ test/source/it/esteco/bowling/GameScoreTest.java
@@ -0,0 +1,17 @@
+package it.esteco.bowling;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameScoreTest {
+
+    @Test
+    public void gutterGame() throws Exception {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+}
