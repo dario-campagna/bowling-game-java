173083c4cfdd3908783afc96e4b3cca3cc0b3713
dcampagna <campagna@esteco.com>
2016-12-01 15:52:48 +0100

    We now compute the score of an all ones game.

--- production/source/it/esteco/bowling/Game.java
+++ production/source/it/esteco/bowling/Game.java
@@ -1,11 +1,14 @@
 package it.esteco.bowling;
 
 public class Game {
-    public void roll(int pins) {
 
+    private int score = 0;
+
+    public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- test/source/it/esteco/bowling/BowlingGameTest.java
+++ test/source/it/esteco/bowling/BowlingGameTest.java
@@ -1,6 +1,5 @@
 package it.esteco.bowling;
 
-import it.esteco.bowling.Game;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,5 +13,14 @@ public class BowlingGameTest {
             g.roll(0);
         }
         assertEquals(0, g.score());
+    }
+
+    @Test
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++) {
+            g.roll(1);
+        }
+        assertEquals(20, g.score());
     }
 }
